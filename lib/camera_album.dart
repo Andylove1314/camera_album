cbee1a65cb190aa1aab741f5fa322c1af4d6b262
OctMon <octmon@qq.com>
2020-12-22 20:58:12 +0800

    删除showPhotoLibrary相关代码

--- lib/camera_album.dart
+++ lib/camera_album.dart
@@ -46,68 +46,10 @@ class CameraAlbum {
   static const String method_switchCamera = 'switchCamera';
   static const String method_setFlashMode = 'setFlashMode';
   static const String method_startCamera = 'startCamera';
-  static const String method_showPhotoLibrary = 'showPhotoLibrary';
-  static const String method_onSelectedHandler = 'onSelectedHandler';
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
-  }
-
-  /// 直接进入相册选择
-  static void showPhotoLibrary(
-      {MediaType mediaType = MediaType.image,
-      int maxSelectCount = 1,
-      String taskTitle = "",
-      String takeTitle = "",
-      String data,
-      void Function(List<CameraAlbumModel>) onSelected,
-      VoidCallback openCamera}) async {
-    _channel.setMethodCallHandler((MethodCall call) async {
-      String method = call.method;
-      Map arguments = call.arguments;
-      print('$method -> $arguments');
-      switch (method) {
-        case method_onSelectedHandler:
-          if (maxSelectCount > 1) {
-            List originPaths = arguments["paths"];
-            List previewPaths = arguments["previewPaths"];
-            List durations = arguments["durations"];
-
-            List<CameraAlbumModel> list = [];
-            for (int index = 0; index < originPaths.length; index++) {
-              list.add(
-                CameraAlbumModel()
-                  ..originPath = originPaths?.isNotEmpty == true
-                      ? '${originPaths[index]}'
-                      : ''
-                  ..previewPath = previewPaths?.isNotEmpty == true
-                      ? '${previewPaths[index]}'
-                      : ''
-                  ..duration = durations?.isNotEmpty == true
-                      ? durations[index] as double
-                      : 0,
-              );
-            }
-            onSelected(list);
-          }
-          break;
-        case method_callCamera:
-          if (openCamera != null) {
-            openCamera();
-          }
-          break;
-        default:
-          throw UnsupportedError("Unrecognized JSON message");
-      }
-    });
-    _channel.invokeMethod(method_showPhotoLibrary, {
-      'mediaType': mediaType.index,
-      'maxSelectCount': maxSelectCount,
-      'taskTitle': taskTitle,
-      'takeTitle': takeTitle,
-      'data': data,
-    });
   }
 
   ///打开相册插件
